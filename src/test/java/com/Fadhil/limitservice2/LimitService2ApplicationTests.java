a592ae50d26dec3e3d3750bfa20bfb0185502c2d
Naga Shirene <61034803+NagaShirene@users.noreply.github.com>
2023-01-04 09:34:55 +0700

    Add files via upload

---
+++ src/test/java/com/Fadhil/limitservice2/LimitService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fadhil.limitservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
